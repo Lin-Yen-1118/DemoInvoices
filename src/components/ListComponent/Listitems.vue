8eab4b63500c472c3d2e3d9ff983ab5bbfdaba31
Lin-Yen-1118 <goatruby@gmail.com>
2022-01-05 15:02:35 +0800

    add popup scss

--- src/components/ListComponent/Listitems.vue
+++ src/components/ListComponent/Listitems.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="listpopup">
     <div class="invoices">
-      <div class="close" @click="closeAdoptForm">
-        <img id="close_icon" src="@/assets/img/close.png" />
-      </div>
       <div class="content">
+        <div class="close" @click="closeAdoptForm">
+          <img id="close_icon" src="@/assets/img/close.png" />
+        </div>
+
         <div class="listpopup_data">
           <div class="tags"><h4>載具</h4></div>
           <div class="top_data">
@@ -12,6 +13,34 @@
             <div class="date"><h3>2021-12-30-12:12:12</h3></div>
             <div class="bargainor"><h4>賣方名稱</h4></div>
           </div>
+        </div>
+        <div class="invoices_item">
+          <!-- 項目列title -->
+          <ul class="data_title">
+            <li class="data_item_name">品項</li>
+            <li class="data_n">數量</li>
+            <li class="data_n">單價</li>
+            <li class="data_n">小計</li>
+          </ul>
+
+          <!-- 要動態新增的資料區塊 -->
+          <ul class="data_body">
+            <li class="data_item_name">品項名稱</li>
+            <li class="data_n">000</li>
+            <li class="data_n">50000</li>
+            <li class="data_n">100000</li>
+          </ul>
+          <ul class="data_body">
+            <li class="data_item_name">品項名稱</li>
+            <li class="data_n">000</li>
+            <li class="data_n">50000</li>
+            <li class="data_n">100000</li>
+          </ul>
+        </div>
+        <!-- 總金額 -->
+        <div class="total_price">
+          <div class="total_amount">總金額</div>
+          <div class="total_amount">2035555</div>
         </div>
       </div>
     </div>
